35911fc02223c94b37e6c0e1d69ee1f67579f322
allfake <allfake24@gmail.com>
2015-09-29 23:40:00 +0700

    add new app

--- src/app/netpie/controllers/netpie-controller.js
+++ src/app/netpie/controllers/netpie-controller.js
@@ -7,7 +7,7 @@
  * Controller of the gulpAngularMqttWs
  */
 angular.module('gulpAngularMqttWs')
-  .controller('netpieCtrl', function ($mdUtil, $mdSidenav, $scope, $http, $localStorage) {
+  .controller('netpieCtrl', function ($mdUtil, $mdSidenav, $scope, $http, $localStorage, $mdToast) {
 
     var buildToggler = function buildToggler(navID) {
       var debounceFn = $mdUtil.debounce(function () {
@@ -27,9 +27,10 @@ angular.module('gulpAngularMqttWs')
         appKey: '2syAvlZPSExXY3M',
         appSecret: 'p5OMOHvdvFaTYSoAx1pvEUZNtD2EW6',
         appId: "HelloChiangMaiMakerClub"
-      }
+      },
+      netpieApp: []
     });
-    
+
     $scope.config = {
       // netpie: $scope.storage.netpie
       netpie: {
@@ -41,12 +42,18 @@ angular.module('gulpAngularMqttWs')
 
     console.log($scope.config);
 
-    $scope.getMqttPass = function () {
+    $scope.setGear = function (gear) {
+      
+      $scope.config.netpie = gear;
+
+    }
+
+    $scope.getMqttPass = function (newApp) {
       console.log("GETTING MQTT AUTH");
       var str = "http://netpie-api.herokuapp.com/api/"
-      str += $scope.storage.netpie.appKey + "/";
-      str += $scope.storage.netpie.appSecret + "/";
-      str += $scope.storage.netpie.appId;
+      str += newApp.appKey + "/";
+      str += newApp.appSecret + "/";
+      str += newApp.appId;
       str += "?callback=JSON_CALLBACK";
       console.log(str);
 
@@ -54,12 +61,49 @@ angular.module('gulpAngularMqttWs')
         .success(function (data) {
           console.log(data);
           $scope.data = data;
+          $scope.loading = false;
+          $scope.appSuccess = "Success";
+
+          var isNewApp = true;
+
+          angular.forEach($scope.storage.netpieApp, function(app, key) {
+            
+            if (app.appId == newApp.appId && 
+                app.appSecret == newApp.appSecret && 
+                app.appKey == newApp.appKey
+                ) 
+            {
+
+              var updateApp = {};
+              updateApp.appId = app.appId;
+              updateApp.appKey = app.appKey;
+              updateApp.appSecret = app.appSecret;
+
+              var microgear = angular.extend(updateApp, data);
+              $scope.storage.netpieApp[key].microgears.push(microgear);
+
+              isNewApp = false;
+              console.log(microgear);
+            }
+
+          });
+
+          if (isNewApp) {
+
+            var microgear = angular.extend({}, newApp, data);
+            newApp.microgears = [];
+            newApp.microgears.push(microgear);
+
+            $scope.storage.netpieApp.push(newApp);
+
+          }
+
         })
         .error(function () {
           console.log("FAILED", arguments);
-
+          $scope.loading = false;
+          $scope.appError = "Failed: " + arguments[1] + " " + arguments[0];
         });
     }
-
 
   });
