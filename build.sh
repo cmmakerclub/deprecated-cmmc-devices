08dfd73a4d523fd19b2bd3255fb12a82efed962c
Nat Weerawan <nat.wrw@gmail.com>
2016-05-22 17:23:38 +0700

    Rebuild

--- build.sh
+++ build.sh
@@ -3,10 +3,12 @@ gulp build
 git add . 
 git commit -m "Rebuild"
 git push 
+rsync -avz dist/ root@cmmc.xyz:/var/www/html/devices/
+
+
 pushd ..
 git commit -am "rebuild"
 git push
 
-rsync -avz dist/ root@cmmc.xyz:/var/www/html/devices/
 popd
 
