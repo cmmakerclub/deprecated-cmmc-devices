6d41d46338cd611f774763cef4f6dfb7542d024e
Nat Weerawan <nat.wrw@gmail.com>
2016-05-23 08:28:49 +0700

    Rebuild

--- build.sh
+++ build.sh
@@ -1,9 +1,12 @@
 rm -rvf dist
 gulp build
+rsync -avz dist/ root@cmmc.xyz:/var/www/html/devices/ 
+
 git add . 
 git commit -m "Rebuild"
 git push 
-rsync -avz dist/ root@cmmc.xyz:/var/www/html/devices/
+
+
 
 
 pushd ..
